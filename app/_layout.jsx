7d76af6a4662ae612092574d4cd456e886e3b93b
SydwellMazibuko <mazibukosydwell@gmail.com>
2026-04-13 15:26:45 +0200

    Implement dark theme support and update layout styles

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,18 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, useColorScheme, View } from 'react-native'
+import React, { use } from 'react'
 import { Slot, Stack } from 'expo-router'
+import { Colours as Colors } from '../constants/Colors'
+import { StatusBar } from 'expo-status-bar'
 
 const RootLayout = () => {
-  return (
+  const colorScheme = useColorScheme()
+  const theme = Colors[colorScheme] ?? Colors.light
 
+  return (
+      <>
+      <StatusBar value="auto"/>
         <Stack screenOptions={{
-          headerStyle: { backgroundColor: '#ddd' },
-          headerTintColor: '#333',
+          headerStyle: { backgroundColor: theme.navBackground },
+          headerTintColor: theme.title,
         }}>
           <Stack.Screen name="index" options={{ title: 'Home' }} />
           <Stack.Screen name="about" options={{ title: 'About' }} />
-          <Stack.Screen name="contact" options={{ title: 'Contact', headerShown: false }} />
+          <Stack.Screen name="contact" options={{ title: 'Contact' }} />
         </Stack>
+      </>
+        
   )
 }
 
